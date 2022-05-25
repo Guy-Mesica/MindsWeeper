919a3f82b91ad570820ed0095b43dd1aaa5c96db
Guy-Mesica <106227929+Guy-Mesica@users.noreply.github.com>
2022-05-25 20:47:18 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const EMPTY = ''
 const MINE = '💣'
 const FLAG = '🏴‍☠️'
 
-
+//////////////////////////////////////
 var gBoard = []
 
 var gGame = {
@@ -14,11 +14,22 @@ var gGame = {
     markedCount: 0,
     secsPassed: 0
 }
-
 var gLevel = {
     size: 4,
     mines: 2
 }
+
+//////////////////////////////////////
+
+function inItGame() {
+    gBoard = buildBoard()
+    renderBoard(gBoard)
+    setMinesNegsCount(gBoard)
+    console.log('gBoard', gBoard);
+    renderBoard(gBoard)
+    console.log('gBoard', gBoard);
+}
+
 
 function buildBoard() {
     var board = []
@@ -27,16 +38,18 @@ function buildBoard() {
         board.push([])
         for (var j = 0; j < gLevel.size; j++) {
             board[i][j] = {
+                idI: i,
+                idJ: j,
                 minesAroundCount: 0,
                 isShown: false,
                 isMine: false,
                 isMarked: false,
             }
-
         }
     }
-    board[2][3].isMine = true
+
     board[0][2].isMine = true
+    board[1][3].isMine = true
     return board
 }
 
@@ -44,23 +57,48 @@ function renderBoard(board) {
     var strHTML = '<tbody class="board">'
 
     for (var i = 0; i < board.length; i++) {
-        strHTML += '<td '
-        
+        strHTML += '<tr>\n '
+        for (var j = 0; j < board[0].length; j++) {
+            if ( board[i][j].isMine) {
+                var cell = MINE
+            } else {
+                cell =board[i][j].minesAroundCount
+            }
+
+            var className = `cell cell-${i}-${j}`
+
+            strHTML += `\t<td class="${className}" onclick="cellClicked(this,${i},${j})">${cell}</td>\n`
+
+        }
+        strHTML += '\n</tr>\n'
+    }
+    strHTML += '</tbody>'
+    var elBoard = document.querySelector('.board-cuntainer')
+    elBoard.innerHTML = strHTML
+}
+
+
+function setMinesNegsCount(board) {
+
+    for (var i = 0; i < board.length; i++) {
+
+        for (var j = 0; j < board[i].length; j++) {
+            board[i][j].minesAroundCount = countNeighbors(i, j, board)
+        }
     }
 
-
-
-
-
-    strHTML += '</tbody>'
+    // return board
 }
 
 
 
 
-function initGame() { }
-function setMinesNegsCount(board) { }
-function cellClicked(elCell, i, j) { }
+
+function cellClicked(elCell, i, j) {
+    console.log(elCell, i, j);
+}
+
+
 function cellMarked(elCell) { }
 function checkGameOver() { }
 function expandShown(board, elCell, i, j) { }
--- js/utils.js
+++ js/utils.js
@@ -1,15 +1,18 @@
 'use strict'
 
-
-
-
-
-
-
-
-
-
-
+function countNeighbors(cellI, cellJ, board) {
+    var minesAroundCount = 0
+    if (board[cellI][cellJ].isMine) return 0
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= board.length) continue;
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            if (i === cellI && j === cellJ) continue;
+            if (j < 0 || j >= board[i].length) continue;
+            if (board[i][j].isMine) minesAroundCount++;
+        }
+    }
+    return minesAroundCount;
+}
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
